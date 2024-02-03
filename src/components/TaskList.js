800431a1ec634d96b00e37b9a27f15e8d396181e
T8ninho <tonisantos.contato@gmail.com>
2024-02-03 03:10:20 -0300

    master

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,50 +2,16 @@ import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons'
 import * as Animatable from 'react-native-animatable';
+import IconButton from './IconButton';
 
 export default function TaskList({ data, handleDelete, handleComplete, handleEdit }){
     return(
-        <Animatable.View 
-          style={styles.container}
-          animation="bounceIn"
-          useNativeDriver
-        >
-              <View style={styles.containerTask}>
-                <TouchableOpacity 
-                  onPress={handleComplete}
-                  style={styles.ButtonConfirm}
-                >
-                  <FontAwesome 
-                    name={data.completed ? "check-square-o" : "square-o"} 
-                    size={30} 
-                    color={data.completed ? "#00ff00" : "#fff"} 
-                  />
-                </TouchableOpacity>
-                <View>
-                  <Text style={data.completed ? styles.taskCompleted : styles.taskIncomplete}>{data.title}</Text>
-                 </View>
-              </View>
+        <Animatable.View  style={styles.container} animation="bounceIn" useNativeDriver >
+              <IconButton onPress={handleComplete} name={data.completed ? "check-square-o" : "square-o"} size={30} color={data.completed ? "#00ff00" : "#fff"} />
+              <Text style={data.completed ? styles.taskCompleted : styles.taskIncomplete}>{data.title}</Text>
               <View style={styles.containerTaskButtons}>
-                <TouchableOpacity 
-                  onPress={handleEdit}
-                  style={styles.ButtonEdit}
-                >
-                  <FontAwesome 
-                    name="edit"
-                    size={30} 
-                    color="#fff"
-                  />
-                </TouchableOpacity>
-                <TouchableOpacity 
-                  onPress={handleDelete}
-                  style={styles.ButtonDelete}
-                >
-                  <FontAwesome 
-                    name="remove"
-                    size={30} 
-                    color="#ec5353"
-                  />
-                </TouchableOpacity>
+                <IconButton onPress={handleEdit} name="edit" color="#fff" style={styles.ButtonEdit} />
+                <IconButton onPress={handleDelete} name="remove" color="#ec5353" />
               </View>
         </Animatable.View>
     )
@@ -63,27 +29,14 @@ const styles = StyleSheet.create({
       padding: 10,
       // elevation: 1,
     },
-    containerTask: {
-      flex: 1,
-      flexDirection: 'row'
-    },
     containerTaskButtons: {
       flex: 1,
       flexDirection: 'row-reverse',
-    },
-    ButtonConfirm: {
-      paddingRight: 8,
-      height: '100%',
-      justifyContent: 'center'
     },
     ButtonEdit: {
       height: '100%',
       justifyContent: 'center',
       paddingLeft: 10,
-    },
-    ButtonDelete: {
-      height: '100%',
-      justifyContent: 'center'
     },
   // Incompletas
     taskIncomplete: {
